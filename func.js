0d2e426eba2a03d6b53ad1e09b55dfe6a2c5f143
unknown <chuyenpham1992@gmail.com>
2022-05-25 23:17:56 +0900

    first commit

--- func.js
+++ func.js
@@ -1,15 +1,13 @@
-function checkAddress(checkbox)
+function darkMode(checkbox)
 {
     if (checkbox.checked)
     {
-        // document.body.style.background = "black"
         document.body.style.color = "white"
         document.getElementById("side-box").style.background = "rgba(169,169,169,0.9)"
         document.getElementById("content").style.background = "rgba(169,169,169,0.9)"
         console.log("checkbox activated");
     }
     else {
-        // document.body.style.background = "lightgray"
         document.body.style.color = "black"
         document.getElementById("side-box").style.background = "rgba(255, 255, 255, 0.8)"
         document.getElementById("content").style.background = "rgba(255, 255, 255, 0.8)"
